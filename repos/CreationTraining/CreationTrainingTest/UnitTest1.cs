76b7093ef359a4bd1be89723e7a7f311398b0381
Ondushi <a_pyagay@mail.ru>
2020-12-20 11:57:16 +0300

    Thou cannot reach Avalon

---
+++ repos/CreationTraining/CreationTrainingTest/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace CreationTrainingTest
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            
+        }
+    }
+}
